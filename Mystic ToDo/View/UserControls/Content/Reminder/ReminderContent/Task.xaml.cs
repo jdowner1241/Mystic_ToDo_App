b5b195454a85aca80abe95c6fe1d0c7ab8c6ccf1
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-11-03 17:28:34 -0500

    Update 0.13 = Code updated, list now populating with the task. But a border needed to be added to Task controler.

--- Mystic ToDo/View/UserControls/Content/Reminder/ReminderContent/Task.xaml.cs
+++ Mystic ToDo/View/UserControls/Content/Reminder/ReminderContent/Task.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Mystic_ToDo.Database;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +23,188 @@ namespace Mystic_ToDo.View.UserControls.Content.Reminder.ReminderContent
     /// <summary>
     /// Interaction logic for Task.xaml
     /// </summary>
-    public partial class Task : UserControl
+    public partial class Task : UserControl, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private int _id;
+        private bool _isComplete;
+        private string _name;
+        private string _description;
+        private DateTime _date;
+        private DateTime _time;
+        private int _frequency; 
+
         public Task()
         {
             InitializeComponent();
         }
+
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set 
+            {
+                _id = value;
+                OnPropertyChanged(nameof(ID));
+            }
+        }
+
+        public bool _IsCompleted
+        {
+            get { return _isComplete; }
+            set 
+            { 
+                _isComplete = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string _Name 
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string _Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime _Date
+        {
+            get { return _date; }
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime _Time
+        {
+            get { return _time; }
+            set 
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int _Frequency
+        {
+            get { return _frequency; }
+            set
+            {
+                _frequency = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public void addInfo(ReminderDb.Reminder newReminder)
+        {
+            if (newReminder.IsComplete == false)
+            {
+                isCompleted.IsChecked = false;
+            }
+            else 
+            {
+                isCompleted.IsChecked = true;
+            }
+
+            name.Content = newReminder.Name;
+
+            if (!string.IsNullOrEmpty(newReminder.Description))
+            {
+                description.Content = newReminder.Description;
+            }
+
+            if (newReminder.HasAlarms == true)
+            {
+                //_Date.Text = newReminder.Alarm.ToString();
+                //_Time.Text = newReminder.Alarm.ToString();
+                date.Text = "Date";
+                time.Text = "Time";
+            }
+            else
+            {
+                date.Text = string.Empty;
+            }
+
+            if (newReminder.Periodic == true)
+            {
+                frequency.Text = "Periodic";
+                //_Frequency.Text = newReminder.TimeFrameSelection.Name.ToString();
+                //newReminder.TimeFrameSelection = DbContext.TimeFrames.Single(tf => tf.TimeFrameId == selectedTimeFrameId);
+            }
+            else
+            {
+               frequency.Text = string.Empty;
+            }
+        }
+
+        public void addInfo1(ReminderDb.Reminder newReminder)
+        {
+            _id = newReminder.Id;
+            _name = newReminder.Name;
+
+            if (newReminder.IsComplete == false)
+            {
+                _isComplete = false;
+            }
+            else
+            {
+                _isComplete = true;
+            }
+
+            if (!string.IsNullOrEmpty(newReminder.Description))
+            {
+               _description = newReminder.Description;
+            }
+            else
+            {
+                _description = string.Empty;
+            }
+
+            if (newReminder.HasAlarms == true)
+            {
+                _date = DateTime.Now;
+                _time = DateTime.Now;
+                //date.Text = "Date";
+                // time.Text = "Time";
+            }
+            else
+            {
+                date.Text = "Date";
+                time.Text = "Time";
+            }
+
+            if (newReminder.Periodic == true)
+            {
+                _frequency = 1;
+            }
+            else
+            {
+                frequency.Text = string.Empty;
+            }
+        }
+
+
     }
 }
--- Mystic ToDo/View/UserControls/Content/Reminder/ReminderPage.xaml.cs
+++ Mystic ToDo/View/UserControls/Content/Reminder/ReminderPage.xaml.cs
@@ -2,8 +2,10 @@
 using Mystic_ToDo.Database;
 using Mystic_ToDo.View.UserControls.Content.Reminder.ReminderContent;
 using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using static Mystic_ToDo.Database.ReminderDb;
@@ -16,29 +18,58 @@ namespace Mystic_ToDo.View.UserControls.Content.Reminder
     public partial class ReminderPage : UserControl
     {
         private readonly ReminderContext DbContext;
+        public ObservableCollection<int> CurrentReminderListIds { get; private set; }
 
         public ReminderPage()
         {
             InitializeComponent();
             DbContext = new ReminderContext();
+            CurrentReminderListIds = new ObservableCollection<int>();
             LoadDataFromReminderPage();
         }
 
+        //Grid version
+        //public void LoadDataFromReminderPage()
+        //{
+        //    Debug.WriteLine("LoadData method invoked");
+
+        //    if (DbContext.Reminders == null)
+        //    {
+        //        MessageBox.Show("Reminders DbSet is null");
+        //    }
+
+        //    var reminderList = DbContext.Reminders.ToList();
+
+        //    reminderListDB.ItemsSource = reminderList;
+        //    Debug.WriteLine("Data loaded successfully");
+        //    reminderListDB.Items.Refresh();
+        //}
+
+
+        //Stackpanel version
         public void LoadDataFromReminderPage()
         {
             Debug.WriteLine("LoadData method invoked");
 
-            if (DbContext.Reminders == null)
-            {
-                MessageBox.Show("Reminders DbSet is null");
-            }
-
             var reminderList = DbContext.Reminders.ToList();
+            TaskList taskList = new TaskList();
+            
 
-            reminderListDB.ItemsSource = reminderList;
+            foreach (var reminder in reminderList)
+            {
+                if (reminder != null) 
+                {
+                    ReminderContent.Task task = new ReminderContent.Task();
+                    task.addInfo1(reminder);
+                    taskList.reminderListDBSub.Children.Add(task);
+                    
+                }
+            }
+            reminderListDB.Children.Add(taskList);
+
             Debug.WriteLine("Data loaded successfully");
-            reminderListDB.Items.Refresh();
         }
+       
 
     }
 }

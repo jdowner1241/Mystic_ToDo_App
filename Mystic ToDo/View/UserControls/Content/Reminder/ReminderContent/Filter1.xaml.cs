e27356fdd02178d07197dcffc4efa40e2c17775b
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-11-29 16:24:28 -0500

    Update 0.24 - Search Button working
    Other requirement
    - Replace search button with a clear button instead

--- Mystic ToDo/View/UserControls/Content/Reminder/ReminderContent/Filter1.xaml.cs
+++ Mystic ToDo/View/UserControls/Content/Reminder/ReminderContent/Filter1.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,50 @@ namespace Mystic_ToDo.View.UserControls.Content.Reminder.ReminderContent
     /// <summary>
     /// Interaction logic for Filter1.xaml
     /// </summary>
-    public partial class Filter1 : UserControl
+    public partial class Filter1 : UserControl, INotifyPropertyChanged
     {
         public Filter1()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private string searchValue;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event Action<string> SearchValueChanged;
+
+        private ReminderPage reminderPage;
+
+        public string SearchValue
+        {
+            get { return searchValue; }
+            set 
+            { 
+                searchValue = value;
+                OnPropertyChanged();
+                SearchValueChanged?.Invoke(value);
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void SubscribeToReminderPageEvents(ReminderPage reminderPage)
+        {
+            this.reminderPage = reminderPage;
+            SearchValueChanged += reminderPage.SearchValueFromReminderPage;
+            //SearchValueChanged += reminderPage.ReminderPageSearch;
+
+            Debug.WriteLine("Subcribe to Filter1 Successful");
+        }
+
+
+        private void BSearch_Click(object sender, RoutedEventArgs e)
+        {
+            SearchValueChanged?.Invoke(SearchValue);
         }
     }
 }

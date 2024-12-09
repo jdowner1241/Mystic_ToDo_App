abf449a80455cf85dfbca39d35b10eedabdb8d3d
jdowner1241 <109806567+jdowner1241@users.noreply.github.com>
2024-12-09 14:33:25 -0500

    Update 0.28 - Login page added - Main Window name updated

---
+++ Mystic ToDo/View/UserControls/Content/LoginPage/LoginPageContent/LoginPageUser.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mystic_ToDo.View.UserControls.Content.LoginPage.LoginPageContent
+{
+    /// <summary>
+    /// Interaction logic for LoginPageUser.xaml
+    /// </summary>
+    public partial class LoginPageUser : UserControl, INotifyPropertyChanged
+    {
+        public LoginPageUser()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

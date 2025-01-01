892ad561138941ffcbe2130229a25f6e31250cf8
Coletb <125518569+Coletb@users.noreply.github.com>
2025-01-01 16:50:20 -0500

    Update 0.36
    - CalenderPage added

--- Mystic ToDo/View/UserControls/Content/Calender/CalenderPage.xaml.cs
+++ Mystic ToDo/View/UserControls/Content/Calender/CalenderPage.xaml.cs
@@ -25,5 +25,47 @@ namespace Mystic_ToDo.View.UserControls.Content.Calender
             DataContext = this;
             InitializeComponent();
         }
+
+
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                //this.DragMove();
+            }
+
+        }
+
+        public void lblNote_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtNote.Focus();
+        }
+
+        private void txtNote_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNote.Text) && txtNote.Text.Length > 0)
+            {
+                lblNote.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lblNote.Visibility = Visibility.Visible;
+            }
+        }
+        private void lblTime_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtNote.Focus();
+        }
+
+        private void txtTime_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtTime.Text) && txtTime.Text.Length > 0)
+
+            { lblTime.Visibility = Visibility.Collapsed; }
+
+            else
+
+            { lblTime.Visibility = Visibility.Visible; }
+        }
     }
 }

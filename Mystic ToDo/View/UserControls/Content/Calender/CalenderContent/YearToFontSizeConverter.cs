429b460cecaa9fe7ccfee11787796381fe41b524
Coletb <125518569+Coletb@users.noreply.github.com>
2025-01-02 19:50:49 -0500

    event for year selection along with 4 other classes created

---
+++ Mystic ToDo/View/UserControls/Content/Calender/CalenderContent/YearToFontSizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Mystic_ToDo.View.UserControls.Content.Calender.CalenderContent
+{
+    internal class YearToFontSizeConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int selectedYear && parameter is int year)
+            {
+                return selectedYear == year ? 24 : 12; // Adjust the font size as needed
+            }
+            return 12;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
